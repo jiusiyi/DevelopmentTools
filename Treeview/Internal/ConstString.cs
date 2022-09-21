5743b14c468bb54d120e344b71e94d17b61624da
jiusiyi <76502504+jiusiyi@users.noreply.github.com>
2022-09-21 09:28:12 +0800

    Add files via upload

--- Treeview/Internal/ConstString.cs
+++ Treeview/Internal/ConstString.cs
@@ -1,1 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace ControlEase.Inspec.TreeView
+{
+    /// <summary>
+    /// Class ConstString
+    /// </summary>
+    internal class ConstString
+    {
+        /// <summary>
+        /// The default full name format
+        /// </summary>
+        public const string DefaultFullNameFormat = "{0}{1}{2}";
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string DefaultFullNameSeparator = "_";
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string NodeDefaultName = "Group";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string VCHDefaultName = "Holder";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string DefaultArea = "DefaultGroup";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string ImageFileFolderSuffix = "P";
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string VideoFileFolderSuffix = "V";
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string LocalRecordFileSuffix = "mp4";
+        //public const string LocalRecordFileSuffix = "txt";
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string USBRecordFileSuffix = "avi";
+
+        /// <summary>
+        /// The save path
+        /// </summary>
+        public const string SavePath = "Video.DAT";
+
+        /// <summary>
+        /// The map default name
+        /// </summary>
+        public const string MapDefaultName = "Map";
+    }
+}
---
+++ Treeview/Internal/SR.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ControlEase.Nexus;
+using System.Threading;
+using System.Globalization;
+
+namespace ControlEase.Inspec.TreeView
+{
+    /// <summary>
+    /// String resources
+    /// </summary>
+    public class SR
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        static readonly IResourceProvider mProvider = new ResourceProvider ( Properties.Resources.ResourceManager );
+
+        /// <summary>
+        /// Get string resource value.
+        /// </summary>
+        /// <param name="name">Resource key.</param>
+        /// <returns>String value.</returns>
+        public static string GetString ( string name )
+        {
+            return mProvider.GetString ( name, Thread.CurrentThread.CurrentUICulture );
+        }
+
+        /// <summary>
+        /// GetString
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public static string GetString ( string name, CultureInfo culture )
+        {
+            return mProvider.GetString ( name, culture );
+        }
+
+        /// <summary>
+        /// Gets the provider.
+        /// </summary>
+        public static IResourceProvider Provider
+        {
+            get { return mProvider; }
+        }
+
+    }
+}
