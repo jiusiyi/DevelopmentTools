68296bef8c4ff0fdb2df12368d448fd4ecf22bb2
jiusiyi <76502504+jiusiyi@users.noreply.github.com>
2022-09-21 09:23:36 +0800

    Add files via upload

--- Treeview/Views/TreeViewControl.xaml.cs
+++ Treeview/Views/TreeViewControl.xaml.cs
@@ -1,1 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
+namespace ControlEase.Inspec.TreeView
+{
+    /// <summary>
+    /// Interaction logic for TreeViewControl.xaml
+    /// </summary>
+    public partial class TreeViewControl : UserControl
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public TreeViewControl ( )
+        {
+            InitializeComponent ( );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserControl_Loaded ( object sender, RoutedEventArgs e )
+        {
+            if ((sender as TreeViewControl).DataContext != null)
+            {
+                ((sender as TreeViewControl).DataContext as TreeViewControlViewModel).TreeViewControl = this;
+                ((sender as TreeViewControl).DataContext as TreeViewControlViewModel).InitializeCommand();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Grid_ContextMenuOpening ( object sender, ContextMenuEventArgs e )
+        {
+            if (( e .OriginalSource as FrameworkElement ).DataContext is TreeNodeViewModel )
+                e.Handled = true;
+        }
+    }
+}
