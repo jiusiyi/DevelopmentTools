c284ff75566380db2e0174d09a62fcfd579ac174
jiusiyi <76502504+jiusiyi@users.noreply.github.com>
2022-09-21 09:18:46 +0800

    Add files via upload

---
+++ Modbus/ModbusConfigData.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ControlEase.IoDrive.Modicon
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ModbusConfigData
+    {
+        #region ... Variables  ...
+        /// <summary>
+        /// 
+        /// </summary>
+        private int mAddress = 1;
+        /// <summary>
+        /// 
+        /// </summary>
+        private CommFormat mCommFormat = CommFormat.RTU;
+        /// <summary>
+        /// 字节转换
+        /// </summary>
+        private Swap mSwap = Swap.SWAP3;
+        #endregion ...Variables...
+
+        #region ... Events     ...
+
+        #endregion ...Events...
+
+        #region ... Constructor...
+
+        #endregion ...Constructor...
+
+        #region ... Properties ...
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public int Address
+        {
+            get
+            {
+                return mAddress;
+            }
+            set
+            {
+                if ( value != mAddress )
+                {
+                    if ( value < 1 || value > 255 )
+                    {
+                        return;
+                    }
+                    mAddress = value;
+                }
+            }
+        }
+        /// <summary>
+        /// 使用的通信协议桢格式
+        /// </summary>
+        public CommFormat CommFormat
+        {
+            get
+            {
+                return mCommFormat;
+            }
+            set
+            {
+                if ( value != mCommFormat )
+                {
+
+                    mCommFormat = value;
+                }
+            }
+        }
+        /// <summary>
+        /// 字节高低位转换寄存器
+        /// </summary>
+        public Swap Swap
+        {
+            get
+            {
+                return mSwap;
+            }
+            set
+            {
+                if ( value != mSwap )
+                {
+
+                    mSwap = value;
+                }
+            }
+        }
+       
+        #endregion ...Properties...
+
+        #region ... Methods    ...
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString ( )
+        {
+            return string.Format ( "{0},{1}", Address.ToString ( ), CommFormat.ToString ( ) );
+        }
+        #endregion ...Methods...
+
+        #region ... Interfaces ...
+
+        #endregion ...Interfaces...
+
+
+    }
+}
---
+++ Modbus/ModbusViewModel.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ControlEase.Nexus.Presentation;
+
+namespace ControlEase.IoDrive.Modicon
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ModbusViewModel : ViewModel
+    {
+        #region ... Variables  ...
+        /// <summary>
+        /// 
+        /// </summary>
+        private ModbusConfigData mConfigData;
+        #endregion ...Variables...
+
+        #region ... Events     ...
+
+        #endregion ...Events...
+
+        #region ... Constructor...
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configData"></param>
+        public ModbusViewModel ( ModbusConfigData configData )
+        {
+            mConfigData = configData;
+        }
+        #endregion ...Constructor...
+
+        #region ... Properties ...
+        #region ...Address...
+        /// <summary>
+        /// 设备地址
+        /// </summary>
+        public int Address
+        {
+            get
+            {
+                return mConfigData.Address;
+            }
+            set
+            {
+                if ( value < 0 || value > 255 )
+                    return;
+                if ( value != mConfigData.Address )
+                {
+                    mConfigData.Address = value;
+                    OnPropertyChanged ( "Address" );
+                }
+            }
+        }
+        #endregion
+        #region ...CommFormat...
+        /// <summary>
+        /// 协议格式为RTU
+        /// </summary>
+        public bool RTUChecked
+        {
+            get
+            {
+                return mConfigData.CommFormat == CommFormat.RTU;
+            }
+            set
+            {
+                if ( value )
+                {
+                    mConfigData.CommFormat = CommFormat.RTU;
+                    OnPropertyChanged ( "RTUChecked" );
+                }
+            }
+        }
+        /// <summary>
+        /// 协议格式为TCP
+        /// </summary>
+        public bool TCPChecked
+        {
+            get
+            {
+                return mConfigData.CommFormat == CommFormat.TCP;
+            }
+            set
+            {
+                if ( value )
+                {
+                    mConfigData.CommFormat = CommFormat.TCP;
+                    OnPropertyChanged ( "TCPChecked" );
+                }
+            }
+        }
+        #endregion
+        #region ...Swap...
+        /// <summary>
+        /// 4字节实型SWAP0
+        /// </summary>
+        public bool SWAP0Checked
+        {
+            get
+            {
+                return mConfigData.Swap == Swap.SWAP0;
+            }
+            set
+            {
+                if ( value )
+                {
+                    mConfigData.Swap = Swap.SWAP0;
+                    OnPropertyChanged ( "SWAP0Checked" );
+                }
+            }
+        }
+        /// <summary>
+        /// 4字节实型SWAP1
+        /// </summary>
+        public bool SWAP1Checked
+        {
+            get
+            {
+                return mConfigData.Swap == Swap.SWAP1;
+            }
+            set
+            {
+                if ( value )
+                {
+                    mConfigData.Swap = Swap.SWAP1;
+                    OnPropertyChanged ( "SWAP1Checked" );
+                }
+            }
+        }
+        /// <summary>
+        /// 4字节实型SWAP2
+        /// </summary>
+        public bool SWAP2Checked
+        {
+            get
+            {
+                return mConfigData.Swap == Swap.SWAP2;
+            }
+            set
+            {
+                if ( value )
+                {
+                    mConfigData.Swap = Swap.SWAP2;
+                    OnPropertyChanged ( "SWAP2Checked" );
+                }
+            }
+        }
+        /// <summary>
+        /// 4字节实型SWAP3
+        /// </summary>
+        public bool SWAP3Checked
+        {
+            get
+            {
+                return mConfigData.Swap == Swap.SWAP3;
+            }
+            set
+            {
+                if ( value )
+                {
+                    mConfigData.Swap = Swap.SWAP3;
+                    OnPropertyChanged ( "SWAP3Checked" );
+                }
+            }
+        }
+
+        #endregion
+        #endregion ...Properties...
+
+        #region ... Methods    ...
+
+        #endregion ...Methods...
+
+        #region ... Interfaces ...
+
+        #endregion ...Interfaces...
+    }
+}
